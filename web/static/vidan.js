1ea9469a1bf3ebcbd0571d0b93c7f1b7b7c0419b
katyauchter <katyauchter@gmail.com>
2014-06-14 01:16:28 -0400

    last for the eve

--- web/static/vidan.js
+++
@@ -1,6 +0,0 @@
-        var numberOfFiles = 1;
-		function addUploadItem () {
-            alert ("xxx");
-			numberOfFiles++;
-			$('#msgid').append("<label for='file'>Filename:</label><input type='file' name='file" + numberOfFiles + "' id='file" + numberOfFiles + "'><br>");
-		}
