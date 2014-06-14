529b2cb22e77c3ac18a27833448be3e5fc49bed3
kcelestine <khadijah.celestine@gmail.com>
2014-06-13 23:45:13 -0700

    done for tonight

--- web/static/js/vidan.js
+++ web/static/js/vidan.js
@@ -4,3 +4,17 @@
 			numberOfFiles++;
 			$('#msgid').append("<label for='file'>Filename:</label><input type='file' name='file" + numberOfFiles + "' id='file" + numberOfFiles + "'><br>");
 		}
+
+
+
+
+function trythis()
+{
+  alert();
+}
+
+function removeLI()
+{
+$(this).closest("li").fadeOut(300);
+
+}
